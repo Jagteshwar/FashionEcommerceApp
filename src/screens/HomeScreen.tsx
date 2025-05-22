a5e6a57ecc07e664a629d94772cbd339b18aa194
Jagteshwar <jagteshwars3@gmail.com>
2025-05-22 11:08:33 -0400

    HomeScreen_WIP

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-
+import LinearGradient from 'react-native-linear-gradient';
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home World!</Text>
-    </View>
+    <LinearGradient
+      colors={['#FDF0F3', '#FFFBFC']}
+      style={styles.container}>
+      <Text style={styles.buttonText}>Sign in with Facebook</Text>
+    </LinearGradient>
   );
 };
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
   },
-  text: {
-    fontSize: 24,
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
     color: 'black',
+    backgroundColor: 'transparent',
   },
 });
 
