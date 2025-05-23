3563a5ba774a9b578e0ac6707f4a67edc86727c4
Jagteshwar <jagteshwars3@gmail.com>
2025-05-23 13:53:40 -0400

    Header component WIP

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,12 +3,13 @@ import {View, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import LinearGradient from 'react-native-linear-gradient';
+import Header from '../components/Header';
 const HomeScreen = () => {
   return (
     <LinearGradient
       colors={['#FDF0F3', '#FFFBFC']}
       style={styles.container}>
-      <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      <Header/>
     </LinearGradient>
   );
 };
